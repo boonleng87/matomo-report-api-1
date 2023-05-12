234877d31a0d102f7dd601df19521c2ba8bf6147
Dexter Teo <>
2023-05-12 15:36:40 +0800

    type fixed for eventmetric struct

--- report/events.go
+++ report/events.go
@@ -6,8 +6,8 @@ type EventsGetMetric struct {
 	Events                 int    `json:"nb_events"`
 	EventsWithValue        int    `json:"nb_events_with_value"`
 	SumEventValue          int    `json:"sum_event_value"`
-	MinEventValue          int    `json:"min_event_value"`
-	MaxEventValue          int    `json:"max_event_value"`
+	MinEventValue          bool   `json:"min_event_value"`
+	MaxEventValue          bool   `json:"max_event_value"`
 	SumDailyNbUniqVisitors int    `json:"sum_daily_nb_uniq_visitors"`
 	AvgEventValue          int    `json:"avg_event_value"`
 	Segment                string `json:"segment"`
